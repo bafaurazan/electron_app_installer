e94d704a6ad74b0c15aa80ec73dd2675323fe790
Rafal Bazan <rafalbe777@gmail.com>
2024-09-08 23:38:50 +0200

    electron builder init and script opening testing

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # electron_app_installer
 This project automates app installation for me to linux and windows from scratch
+
+## to create installer
+`npm run build`
+
+## to install app open 
+
+somethink like that
+
+`dist/my_app Setup 0.0.1.exe`
+
+## to open app open
+
+`dist/win-unpacked/my_app.exe`
+
+or
+
+`selected installation path`
+
+
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools: false,
+        }
+    });
+
+    mainWindow.loadFile('src/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
